8e041b82f39b4c72003d2dccdb1de9d573c01782
jseo79 <joseph79seo@gmail.com>
2024-02-22 15:01:20 -0600

    simplified scroll using scrollIntoView

--- script.js
+++ script.js
@@ -4,20 +4,12 @@ const navigationSmoothScroll = function () {
 	document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 		anchor.addEventListener('click', function (e) {
 			e.preventDefault();
-			console.log(anchor);
 
 			const targetId = this.getAttribute('href').substring(1);
 			const targetElement = document.getElementById(targetId);
 
 			if (targetElement) {
-				const navbarHeight =
-					document.querySelector('.navbar').offsetHeight;
-				const targetOffset = targetElement.offsetTop - navbarHeight;
-
-				window.scrollTo({
-					top: targetOffset,
-					behavior: 'smooth',
-				});
+				targetElement.scrollIntoView({ behavior: 'smooth' });
 			}
 		});
 	});
